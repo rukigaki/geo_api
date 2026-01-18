3d7bd7e090b67a2125abd5665b6b60106370031c
rukigaki <rukigaki@gmail.com>
2026-01-18 18:54:20 +0300

    Created Model messages + file migration

--- points/models.py
+++ points/models.py
@@ -18,3 +18,23 @@ class Point(models.Model):
 
     def __str__(self):
         return f"Point #{self.id}"
+
+
+class Message(models.Model):
+    point = models.ForeignKey(
+        "Point",
+        related_name="messages",
+        on_delete=models.CASCADE,
+    )
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="messages"
+    )
+
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message #{self.id} by {self.author.username}"
