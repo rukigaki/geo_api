5b08d195c8f489d8f5f9c4507b5ff19e4c5b0599
rukigaki <rukigaki@gmail.com>
2026-01-18 19:02:03 +0300

    Created all views for each endpoints

--- points/views.py
+++ points/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Point, Message
+from .serializers import PointSerializer, MessageSerializer
+from .utils import get_points_in_radius
+
+
+class PointAPIView(generics.CreateAPIView):
+    queryset = Point.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = PointSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PointSearchListAPIView(generics.ListAPIView):
+    serializer_class = PointSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return get_points_in_radius(self.request)
+
+
+class PointMessageAPIView(generics.CreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class MessageSearchAPIView(generics.ListAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        points_in_radius = get_points_in_radius(self.request)
+        return Message.objects.filter(point__in=points_in_radius)
